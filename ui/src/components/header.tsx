f61591ad73961505bf393f9b1209850034129dbb
Geordin Raganold <91495472+GRaganold@users.noreply.github.com>
2022-09-13 11:00:05 -0400

    add government of Canada logo (#18)

--- ui/src/components/header.tsx
+++ ui/src/components/header.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 
-import { Box } from "@chakra-ui/react";
 
+import { Box, Center, Image } from "@chakra-ui/react";
 
 export default function TopHeader(){
 
+   
     return(
         <>
         
-        <Box bg='white' w='100%' p={4} color='#202020'>
-
+        <Box bg='white' w='100%'  color='#202020'>
+<Center>
+   <Image src={require('./government_canada_logo_en.png')}  w='15%' />
+</Center>
 
         </Box>
+        
         </>
     )
 }
