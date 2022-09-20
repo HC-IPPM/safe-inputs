e44afac87ee4bf707033e78f1bba7792a25e08f8
Geordin Raganold <91495472+GRaganold@users.noreply.github.com>
2022-09-20 14:43:58 -0400

    insert new feature (translations potential) (#19)

--- ui/src/components/header.tsx
+++ ui/src/components/header.tsx
@@ -1,24 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 
 
-import { Box, Center, HStack, Image, Text } from "@chakra-ui/react";
+import { MinusIcon, AddIcon } from '@chakra-ui/icons'
+import { Accordion, AccordionButton, AccordionItem, AccordionPanel, Box, Flex, Image, Link, Table, TableCaption, TableContainer, Tr, } from "@chakra-ui/react";
 
-export default function TopHeader(){
-
+export default function TopHeader() {
     const logo_en = require('./gov_gouv_en.png')
     const logo_fr = require('./gov_gouv_fr.png')
-   
+    const [language, setLanguage] = useState("en");
+    const handleLanguage = (language: React.SetStateAction<string>) => (event: any) => {
+        setLanguage(language);
+    };
 
-    return(
+
+    return (
         <>
-        
-        <Box bg='white' w='100%'  color='#202020'>
-<Center>
-   <Image src={logo_en} alt={'government_canada_logo_en'}  h={'35px'} m={1} />
-</Center>
+            <Box bg='white' w='100%' color='#202020' fontSize='sm'>
+                <Flex justify='flex-end' mr={30} color='blue' _hover={{ color: 'purple', textDecor: 'underline' }} >
+                    {language === 'en' ? (<><Link onClick={handleLanguage('fr')} > Français</Link></>) : (<><Link onClick={handleLanguage('en')}> English</Link></>)}
+                </Flex>
+                <Flex justify='Center' >
+                    {language === 'en' ? (<><Image src={logo_en} alt={'government_canada_logo_en'} h={'35px'} m={1} /></>) : (<><Image src={logo_fr} alt={'government_canada_logo_fr'} h={'35px'} m={1} /></>)}
+                </Flex>
 
-        </Box>
-        
+                <Accordion allowToggle  defaultIndex={[0]} fontFamily="Noto Sans" fontSize={'16'} color="#333"> 
+                <AccordionItem>
+    {({ isExpanded }) => (
+      <>
+        <h2>
+          <AccordionButton>
+            
+            {isExpanded ? (
+              <><Box flex='1' textAlign='left'>
+              Show Less 
+              </Box> <MinusIcon fontSize='12px' /></>
+            ) : (
+                <><Box flex='1' textAlign='left'>
+                Show More
+                </Box> <AddIcon fontSize='12px' /></>
+            )}
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4} > 
+        <TableContainer>
+              <Table variant="simple">
+                <TableCaption>File properties</TableCaption>
+                <Tr>
+                    'Application'
+                    {language === 'en' ? (<>'Application'</>) : (<> frnech</>)} 
+   
+                    'SheetNames'
+                </Tr>
+                <Tr>
+                    'AppVersion'
+                    'ContentStatus'
+                </Tr>
+                <Tr>
+                    'Title'
+                    'Subject'
+                </Tr>
+                <Tr>
+                    'Author'
+                    'Manager'
+                </Tr>
+                <Tr>
+                    'Company'
+                    'Category'
+                </Tr>
+                <Tr>
+                    'Keywords'
+                    'Comments'
+                </Tr>
+                <Tr>
+                    'LastAuthor'
+                    'CreatedDate'
+                </Tr>
+                <Tr>
+                    'DocSecurity'
+                    'Identifier'
+                </Tr>
+                <Tr>
+                    'SharedDoc'
+                    'Language'
+                </Tr>
+                <Tr>
+                    'HyperlinksChanged'
+                    'Version'
+                </Tr>
+                <Tr>
+                    'LinksUpToDate'
+                    'Revision'
+                </Tr>
+                <Tr>
+                    'ScaleCrop'
+                    'LastPrinted'
+                </Tr>
+                <Tr>
+                    'Worksheets'
+                    'ModifiedDate'
+                </Tr>
+              </Table>
+            </TableContainer>
+        </AccordionPanel>
+      </>
+    )}
+  </AccordionItem>
+</Accordion>
+            </Box>
         </>
     )
 }
+
