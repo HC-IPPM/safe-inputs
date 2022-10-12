6507b192f57dcb7f6af5b6d58949d237faabe2d7
GeordinR <geordinraganold@gmail.com>
2022-10-12 08:36:22 -0400

    add new directory setup. add new page. add css and style

--- ui/src/topHeader.tsx
+++ ui/src/topHeader.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
+import './App.css'
 import './i18n'
 import { HStack, Box, Image, Text, Link } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
-import { FcHome } from "react-icons/fc";
+
 
 export default function TopHeader1() {
 
@@ -12,23 +13,28 @@ export default function TopHeader1() {
     return (
         <>
             <Box >
-                <HStack justify={'space-around'} h='60px' className="pagebody">
+                <HStack justify={'space-around'} h='60px' className="pagebody" id='pageMarginSetting'>
                     <Box></Box>
-                    <Image src={t('safe_inputs.image')} w={{base:'300px', md: '400px' }} min-width={'200px'} />
-                    <Box fontSize={{base:'10px', md: '14px' }} h={{base:'30px', md: '45px' }} >                        
-                            {i18n.language === 'en' ? (<><Link href='' color='blue' defaultValue={i18n.language} onClick={() => i18n.changeLanguage('fr')}
-                        > Français</Link></>) : (<><Link href='' color='blue' defaultValue={i18n.language} onClick={() => i18n.changeLanguage('en')}
-                        >English</Link> </>)} 
-                     
-                       
+                    <Image src={t('safe_inputs.image')} w={{ base: '300px', md: '400px' }} min-width={'200px'} />
+                    <Box fontSize={{ base: '12px', md: '14px' }} h={{ base: '30px', md: '45px' }} >
+                        {i18n.language === 'en' ? (<><Link href='' defaultValue={i18n.language} onClick={() => i18n.changeLanguage('fr')}
+                        > Français</Link></>) : (<><Link href='' defaultValue={i18n.language} onClick={() => i18n.changeLanguage('en')}
+                        >English</Link> </>)}
                     </Box>
                 </HStack>
-                <HStack justify={'space-around'} bg='gray.300'>
-                    <Box></Box>
-                    <Link href='/' ><HStack><FcHome /><Text>{t("menu.home")}</Text></HStack></Link>
-                    <Link href='/secondpage'> {t("menu.secondPage")}</Link>
-                    <Box></Box>
-                </HStack>
+                <Box bg='#EEEEEE' >
+                    <Box id='pageMarginSetting' bg='transparent' padding={'6px 8px'} fontFamily="Noto Sans" >
+                        <HStack gap={3} fontFamily={"Noto Sans"} fontSize='19px' lineHeight='1.4375' h='40px' color='#0B0C0C'>
+                            <Box >
+                                <Link href='/' >{t("menu.home")}</Link>
+                            </Box><Box >
+                                <Link href='/secondpage'> {t("menu.secondPage")}</Link>
+                            </Box><Box>
+                                <Link href='/thirdpage'> {t("menu.thirdPage")}</Link>
+                            </Box>
+                        </HStack>
+                    </Box>
+                </Box>
             </Box>
         </>
     )
