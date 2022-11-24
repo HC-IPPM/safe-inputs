755fc7dd579c1d71763eb37a653779e1c799555a
Lila Kelland <lila.kelland@gmail.com>
2022-11-24 14:07:56 -0500

    changed the nats server to the nats.io demo server rather than using ngs.

--- api/index.js
+++ api/index.js
@@ -6,16 +6,16 @@ import { connect, JSONCodec, jwtAuthenticator } from 'nats'
 const {
   PORT = 3000,
   HOST = '0.0.0.0',
-  // NATS_URL = "demo.nats.io:4222", // Uncomment this to use demo server
-  NATS_URL = "tls://connect.ngs.global:4222", // Comment this out to use demo server
+  NATS_URL = "demo.nats.io:4222", // Uncomment this to use demo server
+  // NATS_URL = "tls://connect.ngs.global:4222", // Comment this out to use demo server
 } = process.env;
 
 // For NATS ngs server connection
-const jwt = process.env.NATS_JWT // Comment this out to use demo server
+// const jwt = process.env.NATS_JWT // Comment this out to use demo server
 
 const nc = await connect({ 
   servers: NATS_URL,
-  authenticator: jwtAuthenticator(jwt), // Comment this out to use demo server
+  // authenticator: jwtAuthenticator(jwt), // Comment this out to use demo server
 })
 
 const jc = JSONCodec(); // for encoding NAT's messages
