867c0142386d77dc2a7bd2f525e158760515adbd
Collin Brown <collin.j.brown95@gmail.com>
2023-08-24 17:31:39 -0400

    feat(navPage.tsx): add footer

--- rspack-ui/src/pages/navPage.tsx
+++ rspack-ui/src/pages/navPage.tsx
@@ -5,12 +5,14 @@ import { Outlet } from 'react-router-dom';
 import Inbox from "../Inbox";
 
 import TopHeader from '../components/Header';
+import Footer from '../components/Footer';
 // This is the component that renders the header and footer for all pages
 export default function NavPage() {
     return (
         <>
             <TopHeader />
             <Inbox />
+            <Footer />
             {/* Outlet function used by React-Router-Dom to nest the child routes/elements when rendered.   */}
             <Outlet />
         </>
