3526db4cc2d30dad8cf4edca2e0446ed94c72f0c
Collin Brown <collin.j.brown95@gmail.com>
2023-08-25 10:33:35 -0400

    chore(nagPage.tsx): remove Inbox component that was used for testing

--- rspack-ui/src/pages/navPage.tsx
+++ rspack-ui/src/pages/navPage.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 import { Outlet } from 'react-router-dom';
 
-import Inbox from "../Inbox";
-
 import TopHeader from '../components/Header';
 import Footer from '../components/Footer';
 // This is the component that renders the header and footer for all pages
@@ -11,10 +9,8 @@ export default function NavPage() {
     return (
         <>
             <TopHeader />
-            <Inbox />
-            <Footer />
-            {/* Outlet function used by React-Router-Dom to nest the child routes/elements when rendered.   */}
             <Outlet />
+            <Footer />
         </>
     )
 }
