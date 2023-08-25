0c600ff10f74bd6f9bbcba594739471f9a7584e9
Collin Brown <collin.j.brown95@gmail.com>
2023-08-25 10:32:30 -0400

    fix(Footer.tsx): use as=ReactRouterLink in Chakra-UI link - rather than trying to load a page with the link, this defers to the React   BrowserRouter instead.

--- rspack-ui/src/components/Footer.tsx
+++ rspack-ui/src/components/Footer.tsx
@@ -9,6 +9,8 @@ import {
     Spacer,
 } from '@chakra-ui/react'
 
+import { Link as ReactRouterLink } from 'react-router-dom'
+
 import { Trans } from '@lingui/macro';
 
 const BoxStyle = {
@@ -69,7 +71,7 @@ export default function Footer() {
                             <SimpleGrid columns={[1, null, 2]} gap={{ base: 3, md: 0 }}>
                                 <Box pt={{ base: 6, md: 20 }}>
                                     <Trans>
-                                        <Link href="/termsConditions" {...LinkStyle}>
+                                        <Link as={ReactRouterLink} to="/termsAndConditions" {...LinkStyle}>
                                             <Text>Terms and Conditions</Text>
                                         </Link>
                                     </Trans>
