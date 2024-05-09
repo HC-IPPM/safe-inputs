95a81713500a82c69a18341d5c9e8f4866e0f8f6
Stephen-ONeil <stephenlevioneil@gmail.com>
2024-05-09 15:21:30 -0400

    Set semicolons to true in /api/.prettierrc, run prettier on the /api dir too

--- api/index.js
+++ api/index.js
@@ -1,22 +1,22 @@
-import 'dotenv/config'
-import { Server } from './src/Server.js'
-import { schema } from './src/schema.js'
-import { connect, JSONCodec, jwtAuthenticator } from 'nats'
+import 'dotenv/config';
+import { Server } from './src/Server.js';
+import { schema } from './src/schema.js';
+import { connect, JSONCodec, jwtAuthenticator } from 'nats';
 
-const { 
+const {
   PORT = 3000,
   HOST = '0.0.0.0',
-  NATS_URL = "demo.nats.io:4222", // Uncomment this to use demo server
+  NATS_URL = 'demo.nats.io:4222', // Uncomment this to use demo server
   // NATS_URL = "tls://connect.ngs.global:4222", // Comment this out to use demo server
 } = process.env;
 
 // For NATS ngs server connection
 // const jwt = process.env.NATS_JWT // Comment this out to use demo server
 
-const nc = await connect({ 
+const nc = await connect({
   servers: NATS_URL,
   // authenticator: jwtAuthenticator(jwt), // Comment this out to use demo server
-})
+});
 
 const jc = JSONCodec(); // for encoding NAT's messages
 
@@ -24,23 +24,23 @@ const jc = JSONCodec(); // for encoding NAT's messages
 const jsm = await nc.jetstreamManager();
 const js = nc.jetstream();
 
-// Add a stream to publish on 
-const stream = "safeInputsRawSheetData";
+// Add a stream to publish on
+const stream = 'safeInputsRawSheetData';
 const subj = `safeInputsRawSheetData`;
 await jsm.streams.add({ name: stream, subjects: [subj] });
 
 function publish(payload) {
-  js.publish(`safeInputsRawSheetData`, jc.encode(payload)) 
+  js.publish(`safeInputsRawSheetData`, jc.encode(payload));
 }
 
-process.on('SIGTERM', () => process.exit(0))
-process.on('SIGINT', () => process.exit(0))
-;(async () => {
+process.on('SIGTERM', () => process.exit(0));
+process.on('SIGINT', () => process.exit(0));
+(async () => {
   const server = new Server({
     schema,
     context: { publish },
-  })
+  });
   server.listen({ port: PORT, host: HOST }, () =>
     console.log(`🚀 Safe-inputs API listening on ${HOST}:${PORT}`),
-  )
-})()
+  );
+})();
--- api/src/__tests__/schema.test.js
+++ api/src/__tests__/schema.test.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line
-import { jest } from '@jest/globals' // support for ESM modules
-import { schema } from '../schema.js'
-import { graphql } from 'graphql'
+import { jest } from '@jest/globals'; // support for ESM modules
+import { schema } from '../schema.js';
+import { graphql } from 'graphql';
 
 describe('schema', () => {
   describe('query', () => {
@@ -10,12 +10,12 @@ describe('schema', () => {
         const response = await graphql({
           schema,
           source: '{hello}',
-        })
+        });
 
-        expect(response).toEqual({ data: { hello: 'world!' } })
-      })
-    })
-  })
+        expect(response).toEqual({ data: { hello: 'world!' } });
+      });
+    });
+  });
 
   describe('mutation', () => {
     describe('verifyJsonFormat with object input', () => {
@@ -24,12 +24,12 @@ describe('schema', () => {
           schema,
           source: `mutation verifyJsonFormat($testObject: JSON!){
             verifyJsonFormat(sheetData: $testObject)}`,
-          variableValues: {"testObject": {name:"John", age:30, car:null}},
-        })
+          variableValues: { testObject: { name: 'John', age: 30, car: null } },
+        });
 
-        expect(typeof response.data.verifyJsonFormat === 'object').toBe(true)
-      })
-    })
+        expect(typeof response.data.verifyJsonFormat === 'object').toBe(true);
+      });
+    });
 
     describe('verifyJsonFormat with string input', () => {
       it('will accept JSON string for SheetData field', async () => {
@@ -38,41 +38,39 @@ describe('schema', () => {
           // source: 'mutation {verifyJsonFormat(sheetData: "a")}',
           source: `mutation verifyJsonFormat($testString: JSON!){
             verifyJsonFormat(sheetData: $testString)}`,
-          variableValues: {"testString": "a"},
-          contextValue: {publish: () => 'this is a string'},
-        })
+          variableValues: { testString: 'a' },
+          contextValue: { publish: () => 'this is a string' },
+        });
 
-        expect(response).toEqual({"data": {"verifyJsonFormat": "a"}})
-        expect(typeof response.data.verifyJsonFormat === 'string').toBe(true)
-      })
-    })
+        expect(response).toEqual({ data: { verifyJsonFormat: 'a' } });
+        expect(typeof response.data.verifyJsonFormat === 'string').toBe(true);
+      });
+    });
 
     describe('verifyJsonFormat with non JSON input', () => {
       it('will result in an error', async () => {
         const response = await graphql({
           schema,
           source: 'mutation {verifyJsonFormat(sheetData: a)}',
-        })
-        const [err] = response.errors
+        });
+        const [err] = response.errors;
 
         expect(err.message).toEqual(
-          "Expected value of type \"JSON!\", found a; JSON cannot represent value: a",
-        )
-      })
-    })
+          'Expected value of type "JSON!", found a; JSON cannot represent value: a',
+        );
+      });
+    });
 
     describe('verifyJsonFormat with null input', () => {
       it('will result in error', async () => {
         const response = await graphql({
           schema,
           source: 'mutation {verifyJsonFormat(sheetData: }',
-        })
-        const [err] = response.errors
-        
-        expect(err.message).toEqual(
-          'Syntax Error: Unexpected \"}\".',
-        )
-      })
-    })
-  })
-})
+        });
+        const [err] = response.errors;
+
+        expect(err.message).toEqual('Syntax Error: Unexpected "}".');
+      });
+    });
+  });
+});
