77ae472d8f9e376cd8dd583369b44eb0783fa1db
Stephen-ONeil <stephenlevioneil@gmail.com>
2024-05-22 16:48:43 -0400

    In dev docker compose environment, create non-root user and non-default DB for the api to connect to

--- api/dev-db-init.js
+++ api/dev-db-init.js
@@ -1,1 +1,16 @@
-// TODO
+const { MDB_USERNAME, MDB_PW, MDB_NAME } = process.env;
+
+api_db = db.getSiblingDB(MDB_NAME);
+
+api_db.createUser({
+  user: MDB_USERNAME,
+  pwd: MDB_PW,
+  roles: [
+    {
+      role: 'readWrite',
+      db: MDB_NAME,
+    },
+  ],
+});
+
+// TODO populate some dev/test data
