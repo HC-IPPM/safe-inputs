67f733297d66795a3c7dbd1fcadbd4bbe13f1b21
Stephen-ONeil <stephenlevioneil@gmail.com>
2024-05-22 17:17:45 -0400

    Add temporary conditionals to disable DB connection attempts in the kubernetes and test environments

--- api/setupTests.js
+++ api/setupTests.js
@@ -2,3 +2,5 @@ import dotenv from 'dotenv';
 import { AbortSignal } from 'node-abort-controller';
 global.AbortSignal = AbortSignal;
 dotenv.config({ path: './.env.dev-public' });
+
+process.env.IS_TEST_ENV = true;
--- api/src/db_utils.js
+++ api/src/db_utils.js
@@ -12,13 +12,17 @@ function get_connection_str() {
 // It's safe to let the connection happen fully async because further mongoose opperations are also async and will
 // buffer until the connection's made
 export async function connect_db() {
-  console.log('Attempting MongoDB connection...');
-  return await mongoose
-    .connect(get_connection_str(), {
-      serverSelectionTimeoutMS: 7500,
-      heartbeatFrequencyMS: 10000,
-    })
-    .then(() => console.log('MongoDB connected!'));
+  // TODO: temporary conditional! the kubernetes environment isn't ready for the api to attempt DB connections,
+  // and tests need to be updated to, most likely, mock away DB connections
+  if (process.env.IS_DEV_ENV && !process.env.IS_TEST_ENV) {
+    console.log('Attempting MongoDB connection...');
+    return await mongoose
+      .connect(get_connection_str(), {
+        serverSelectionTimeoutMS: 7500,
+        heartbeatFrequencyMS: 10000,
+      })
+      .then(() => console.log('MongoDB connected!'));
+  }
 }
 
 export function get_db_connection_status() {
