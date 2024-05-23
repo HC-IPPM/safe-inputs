a81c0660b0a2cad62f0e3ed3e21ede238c12e164
Stephen-ONeil <stephenlevioneil@gmail.com>
2024-05-23 11:41:33 -0400

    Remove async IIF in api/index.js, not necessary

--- api/index.js
+++ api/index.js
@@ -9,24 +9,20 @@ const { PORT = 3000, HOST = '0.0.0.0' } = process.env;
 process.on('SIGTERM', () => {
   throw new Error('SIGTERM');
 });
-
 process.on('SIGINT', () => {
   throw new Error('SIGINT');
 });
 
-(async () => {
-  const server = new Server({
-    schema,
-    context: {},
-  });
+const server = new Server({
+  schema,
+  context: {},
+});
 
-  // priming the DB connection, ok if this fails, will reassert connection before handling
-  // any given request
-  connect_db().catch((err) => {
-    console.error(err);
-  });
+// priming the DB connection (async), ok if this fails, will reassert connection before handling any given request
+connect_db().catch((err) => {
+  console.error(err);
+});
 
-  server.listen({ port: PORT, host: HOST }, () =>
-    console.log(`🚀 Safe-inputs API listening on ${HOST}:${PORT}`),
-  );
-})();
+server.listen({ port: PORT, host: HOST }, () =>
+  console.log(`🚀 Safe-inputs API listening on ${HOST}:${PORT}`),
+);
