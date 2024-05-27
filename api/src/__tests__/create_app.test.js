c1bfb8e716b31c073ef94c48385d3995b439053a
Stephen-ONeil <stephenlevioneil@gmail.com>
2024-05-27 19:44:34 -0400

    Close DB connections after each test in create_app.test.js, for isolation but most importantly so that after the last test runs Jest isn't held open by a hanging DB handler. Remove --detectOpenHandler flag from api jest shorthand

--- api/src/__tests__/create_app.test.js
+++ api/src/__tests__/create_app.test.js
@@ -1,4 +1,5 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
+import mongoose from 'mongoose';
 import request from 'supertest'; // eslint-disable-line node/no-unpublished-import
 
 import { create_app } from '../create_app.js';
@@ -35,6 +36,11 @@ const schema = makeExecutableSchema({ typeDefs, resolvers });
 // ----- TESTS -----
 
 describe('create_app', () => {
+  afterEach(() => {
+    // necessary cleanup to prevent Jest from being held open by a dangling DB handler
+    return mongoose.connection.close();
+  });
+
   describe('given a schema and resolver', () => {
     it('returns an express app', async () => {
       const app = await create_app({ schema });
