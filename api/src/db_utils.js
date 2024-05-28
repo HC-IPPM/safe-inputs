d09c836c8296071ae2d6a558d559080e363a73dd
Stephen-ONeil <stephenlevioneil@gmail.com>
2024-05-28 17:12:43 -0400

    Set up k8s api env vars for DB connection and auth.js. Remove the conditional to skip DB connections when not in the local environment

--- api/src/db_utils.js
+++ api/src/db_utils.js
@@ -1,25 +1,14 @@
 import mongoose from 'mongoose';
 
-function get_connection_str() {
-  const { MDB_CONNECT_STRING, MDB_USERNAME, MDB_PW, MDB_NAME } = process.env;
-
-  return MDB_CONNECT_STRING.replace('{MDB_USERNAME}', MDB_USERNAME)
-    .replace('{MDB_PW}', MDB_PW)
-    .replace('{MDB_NAME}', MDB_NAME);
-}
-
 export async function connect_db() {
-  // TODO: temporary conditional! the kubernetes environment isn't ready for the api to attempt DB connections yet
-  if (process.env.IS_LOCAL_ENV) {
-    queueMicrotask(() => console.log('Attempting MongoDB connection...'));
+  queueMicrotask(() => console.log('Attempting MongoDB connection...'));
 
-    return await mongoose
-      .connect(get_connection_str(), {
-        serverSelectionTimeoutMS: 7500,
-        heartbeatFrequencyMS: 10000,
-      })
-      .then(() => queueMicrotask(() => console.log('MongoDB connected!')));
-  }
+  return await mongoose
+    .connect(process.env.MDB_CONNECT_STRING, {
+      serverSelectionTimeoutMS: 7500,
+      heartbeatFrequencyMS: 10000,
+    })
+    .then(() => queueMicrotask(() => console.log('MongoDB connected!')));
 }
 
 export function get_db_client() {
