2a121e7c06dd903ce3f481a278603f159f0ffdd6
Stephen-ONeil <stephenlevioneil@gmail.com>
2024-06-17 18:58:39 -0400

    Add envalid to the api, for env value validation and centralized defaults. Cleanup related to env value consumption

--- api/src/db_utils.js
+++ api/src/db_utils.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
+import { get_env } from './env_utils.js';
 
 export async function connect_db() {
+  const { MDB_CONNECT_STRING } = get_env();
+
   queueMicrotask(() => console.log('Attempting MongoDB connection...'));
 
   return await mongoose
-    .connect(process.env.MDB_CONNECT_STRING, {
+    .connect(MDB_CONNECT_STRING, {
       serverSelectionTimeoutMS: 7500,
       heartbeatFrequencyMS: 10000,
     })
