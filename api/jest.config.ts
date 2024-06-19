bba37ddcc7f8470a5ac93d722221c2e36b91b0e6
Stephen-ONeil <stephenlevioneil@gmail.com>
2024-06-19 18:06:19 -0400

    Bunch more linter/typescript/jest configuration tweaks, for consistency and to standardize (and fix, in the /api service) on absolute  imports resolving correctly

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -1,11 +1,17 @@
+import { pathsToModuleNameMapper } from 'ts-jest'; // eslint-disable-line node/no-unpublished-import
+
+import { compilerOptions } from './tsconfig.json';
+
 export default {
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  setupFilesAfterEnv: ['./src/setup_tests.ts'],
+  preset: 'ts-jest',
+  roots: ['<rootDir>'],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>',
+  }),
+
   verbose: true,
 
-  moduleNameMapper: {
-    '^@src/(.*)$': './src/$1',
-  },
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['./src/setup_tests.ts'],
 };
