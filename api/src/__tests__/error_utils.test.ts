a9851e9c238d9aab6ef13b8ad7df0ee9a1498c45
Stephen Levi O'Neil <stephenlevioneil@gmail.com>
2024-06-28 13:34:00 -0400

    API error handling (#182)
    * For consistency with express.js, rename AppError's statusCode property to status
    
    * Add custom errorHandler middleware to api, return error messages as json rather than HTML
    
    * Add tests for errorHandler middlewear
    
    * Add DEV_IS_TEST_ENV to get_env return, use (for now) to silence unnecessary errorHandler logging during tests

--- api/src/__tests__/error_utils.test.ts
+++ api/src/__tests__/error_utils.test.ts
@@ -1,14 +1,81 @@
-import { AppError } from 'src/error_utils.ts';
+import express from 'express';
+import request from 'supertest'; // eslint-disable-line node/no-unpublished-import
+
+import { AppError, errorHandler } from 'src/error_utils.ts';
 
 describe('AppError', () => {
-  it('constructs a new error with the provided message and the additional statusCode property', () => {
+  it('constructs a new error with the provided message and the additional status property', () => {
     const message = 'test message for testing the message';
-    const statusCode = 123;
+    const status = 123;
 
-    const appError = new AppError(statusCode, message);
+    const appError = new AppError(status, message);
 
     expect(appError).toBeInstanceOf(Error);
     expect(appError.message).toBe(message);
-    expect(appError.statusCode).toBe(statusCode);
+    expect(appError.status).toBe(status);
+  });
+});
+
+describe('errorHandler middlewear', () => {
+  it('handles errors passed to next() by express routes', async () => {
+    const app = express();
+
+    const status_code = 500;
+
+    app.get('/error-via-next', function (_req, _res, next) {
+      next(new AppError(status_code, 'error'));
+    });
+
+    const mockedErrorHandler = jest.fn(errorHandler);
+    app.use(mockedErrorHandler);
+
+    const response = await request(app)
+      .get('/error-via-next')
+      .set('Accept', 'application/json');
+
+    expect(mockedErrorHandler).toHaveBeenCalledTimes(1);
+    expect(response.status).toBe(status_code);
+  });
+
+  it('handles errors thrown by express routes', async () => {
+    const app = express();
+
+    const status_code = 500;
+
+    app.get('/error-via-next', function (_req, _res, _next) {
+      throw new AppError(status_code, 'error');
+    });
+
+    const mockedErrorHandler = jest.fn(errorHandler);
+    app.use(mockedErrorHandler);
+
+    const response = await request(app)
+      .get('/error-via-next')
+      .set('Accept', 'application/json');
+
+    expect(mockedErrorHandler).toHaveBeenCalled();
+    expect(response.status).toBe(status_code);
+  });
+
+  it('sets the response status based on the error, includes the error message in the response body', async () => {
+    const app = express();
+
+    const status_code = 418;
+    const error_message = "I'm a teapot";
+
+    app.get('/error', function (_req, _res, _next) {
+      throw new AppError(status_code, error_message);
+    });
+
+    const mockedErrorHandler = jest.fn(errorHandler);
+    app.use(mockedErrorHandler);
+
+    const response = await request(app)
+      .get('/error')
+      .set('Accept', 'application/json');
+
+    expect(mockedErrorHandler).toHaveBeenCalled();
+    expect(response.status).toBe(status_code);
+    expect(response.body.error).toBe(error_message);
   });
 });
--- api/src/error_utils.ts
+++ api/src/error_utils.ts
@@ -1,14 +1,39 @@
-export type AppErrorInstance = Error & { statusCode: number };
+import type { Request, Response, NextFunction } from 'express';
+
+import { get_env } from './env.ts';
+
+export type AppErrorInstance = Error & { status: number };
 
 interface AppErrorConstructor {
-  (statusCode: number, message: string): AppErrorInstance;
-  new (statusCode: number, message: string): AppErrorInstance;
+  (status: number, message: string): AppErrorInstance;
+  new (status: number, message: string): AppErrorInstance;
 }
 
-export const AppError = function (statusCode: number, message: string) {
+export const AppError = function (status: number, message: string) {
   const error = new Error(message) as AppErrorInstance;
 
-  error.statusCode = statusCode;
+  // standard in express.js to add an HTTP status code on errors, as `status`, to be used by error handling middleware/error responses
+  // both when throwing errors and when passing them to an express `next` call
+  error.status = status;
 
   return error;
 } as AppErrorConstructor;
+
+export const errorHandler = (
+  err: Error | AppErrorInstance,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  const { DEV_IS_TEST_ENV } = get_env();
+
+  if (!DEV_IS_TEST_ENV) {
+    // TODO: a good structured logging library and logging utils are a TODO
+    // console output should be silenced when running tests, for clean test reports
+    console.error(err.stack);
+  }
+
+  const status_code = 'status' in err ? err.status : 500;
+
+  res.status(status_code).json({ error: err.message });
+};
