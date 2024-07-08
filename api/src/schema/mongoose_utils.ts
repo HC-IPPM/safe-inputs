635270bad83c6d523e1e88f73a630ba6d5e3c720
Stephen-ONeil <stephenlevioneil@gmail.com>
2024-07-08 11:39:09 -0400

    Add Mongoose utils (schema types and schema helpers)

---
+++ api/src/schema/mongoose_utils.ts
@@ -0,0 +1,42 @@
+export const str_type = { type: String };
+
+export const number_type = { type: Number };
+
+export const pkey_type = () => ({
+  ...str_type,
+  required: true,
+  unique: true,
+  index: true,
+});
+
+// This probably shouldn't be used outside of the case of optional subdocuments, but it IS required for subdocuments whenever
+// the subdocument schema needs a pkey but isn't itself a required field of the parent. If multiple parents ARE
+// missing a subdocument which has a required unique index then the database will throw a "dup key: { : null }" error.
+// The only solution is to not put unique indexes on the subdocuments or to make sure they're sparse.
+export const sparse_pkey_type = () => ({
+  ...str_type,
+  sparse: true,
+  unique: true,
+  index: true,
+});
+
+export const parent_fkey_type = () => ({
+  ...str_type,
+  required: true,
+  index: true,
+});
+
+export const sparse_parent_fkey_type = () => ({
+  ...str_type,
+  sparse: true,
+  index: true,
+});
+
+export const MakeBilingualType = <Name extends string, MongooseType>(
+  name: Name,
+  type: MongooseType,
+) =>
+  ({
+    [`${name}_en`]: type,
+    [`${name}_fr`]: type,
+  }) as { [k in `${Name}_en` | `${Name}_fr`]: MongooseType };
