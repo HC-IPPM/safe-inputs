92a76ed6b44cb49e3ee749b9074246bea4ff6610
Stephen-ONeil <stephenlevioneil@gmail.com>
2024-07-09 18:38:32 -0400

    Add utility for converting api AppError instances in to GraphQLError, for utilities that may be called in either context (express route or during yoga request resolution)

--- api/src/error_utils.ts
+++ api/src/error_utils.ts
@@ -1,5 +1,7 @@
 import type { Request, Response, NextFunction } from 'express';
 
+import { GraphQLError } from 'graphql';
+
 import { get_env } from './env.ts';
 
 export type AppErrorInstance = Error & { status: number };
@@ -19,12 +21,7 @@ export const AppError = function (status: number, message: string) {
   return error;
 } as AppErrorConstructor;
 
-export const errorHandler = (
-  err: Error | AppErrorInstance,
-  _req: Request,
-  res: Response,
-  _next: NextFunction,
-) => {
+const handle_error_logging = (err: Error | AppErrorInstance) => {
   const { DEV_IS_TEST_ENV } = get_env();
 
   if (!DEV_IS_TEST_ENV) {
@@ -32,8 +29,35 @@ export const errorHandler = (
     // console output should be silenced when running tests, for clean test reports
     console.error(err.stack);
   }
+};
 
-  const status_code = 'status' in err ? err.status : 500;
+const get_status_code = (err: Error | AppErrorInstance) =>
+  'status' in err ? err.status : 500;
 
-  res.status(status_code).json({ error: err.message });
+export const expressErrorHandler = (
+  err: Error | AppErrorInstance,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  handle_error_logging(err);
+
+  res.status(get_status_code(err)).json({ error: err.message });
+};
+
+export const app_error_to_gql_error = (err: Error | AppErrorInstance): void => {
+  // yoga catches errors thrown while resloving GQL requests. These errors don't reach the express error handler, instead yoga
+  // processes them and includes them in the `errors` field of the GQL response, as is expected for GraphQL. Yoga only includes messages
+  // from GraphQLError instances in the client response, but we want AppError thrown by our common utils to be reported to the client too
+  // See here for more on why: https://the-guild.dev/graphql/yoga-server/tutorial/basic/09-error-handling#exposing-safe-error-messages
+
+  if (err instanceof AppError) {
+    throw new GraphQLError(err.message, {
+      extensions: {
+        code: get_status_code(err),
+      },
+    });
+  } else {
+    throw err;
+  }
 };
--- api/src/schema/resolver_utils.ts
+++ api/src/schema/resolver_utils.ts
@@ -3,7 +3,7 @@ import _ from 'lodash';
 import { apply_rules_to_user, validate_user_email_allowed } from 'src/authz.ts';
 import type { AuthzRule } from 'src/authz.ts';
 
-import { AppError } from 'src/error_utils.ts';
+import { AppError, app_error_to_gql_error } from 'src/error_utils.ts';
 
 import type { LangsUnion, BilingualKeyUnion } from './lang_utils.ts';
 
@@ -22,18 +22,27 @@ export const with_authz =
     ...authz_rules: AuthzRule[]
   ): ((parent: Parent, args: Args, context: Context) => Result) =>
   (parent: Parent, args: Args, context: Context) => {
-    if (typeof context?.req?.user === 'undefined') {
-      throw new AppError(
-        401,
-        'Query contains fields requiring an authenticated session. No session found.',
-      );
-    }
+    try {
+      if (typeof context?.req?.user === 'undefined') {
+        throw new AppError(
+          401,
+          'Query contains fields requiring an authenticated session. No session found.',
+        );
+      }
 
-    apply_rules_to_user(
-      context.req.user,
-      validate_user_email_allowed,
-      ...authz_rules,
-    );
+      // may throw an AppError, which will need to be converted to a GraphQLError per
+      // https://the-guild.dev/graphql/yoga-server/tutorial/basic/09-error-handling#exposing-safe-error-messages
+      apply_rules_to_user(
+        context.req.user,
+        validate_user_email_allowed,
+        ...authz_rules,
+      );
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      debugger;
+      app_error_to_gql_error(error);
+    }
 
     return resolver(parent, args, context);
   };
