1e6b545bfb4a256cad5867bebb8aed1d83fbe536
Stephen-ONeil <stephenlevioneil@gmail.com>
2024-07-16 12:05:16 -0400

    More fleshed out DatasetRules and DatasetRecords models

--- api/src/schema/core/DatasetRecords/DatasetRecordsModel.ts
+++ api/src/schema/core/DatasetRecords/DatasetRecordsModel.ts
@@ -2,34 +2,29 @@ import { Schema, model, Types } from 'mongoose';
 import type { Document } from 'mongoose';
 
 import { create_dataloader_for_resource_by_primary_key_attr } from 'src/schema/loader_utils.ts';
+import { make_foreign_id_type } from 'src/schema/mongoose_utils.ts';
 
-import {
-  make_foreign_id_type,
-  make_foreign_key_type,
-} from 'src/schema/mongoose_utils.ts';
+interface RecordInterface extends Document<Types.ObjectId> {
+  data: Record<string, any>;
+  created_by: Types.ObjectId;
+  created_at: number;
+}
+const RecordSchema = new Schema<RecordInterface>({
+  data: Schema.Types.Mixed,
+  created_by: make_foreign_id_type('User', { required: true }),
+  created_at: { type: Number, required: true },
+});
 
 interface DatasetRecordsInterface extends Document<Types.ObjectId> {
-  dataset_stable_key: string;
-  rules: Types.ObjectId;
-  data: any[];
+  records: RecordInterface[];
 }
-const DatasetMongooseSchema = new Schema<DatasetRecordsInterface>({
-  dataset_stable_key: make_foreign_key_type('Dataset', {
-    required: true,
-  }),
-  rules: make_foreign_id_type('DatasetRules', {
-    required: true,
-  }),
-  data: [Schema.Types.Mixed],
+const DatasetRecordsMongooseSchema = new Schema<DatasetRecordsInterface>({
+  records: [RecordSchema],
 });
-DatasetMongooseSchema.index(
-  { dataset_stable_id: 1, rules: 1 },
-  { unique: true },
-);
 
 export const DatasetRecordsModel = model<DatasetRecordsInterface>(
   'DatasetRecords',
-  DatasetMongooseSchema,
+  DatasetRecordsMongooseSchema,
 );
 
 export const DatasetRecordsByIdLoader =
--- api/src/schema/core/DatasetRules/DatasetRulesModel.ts
+++ api/src/schema/core/DatasetRules/DatasetRulesModel.ts
@@ -1,13 +1,51 @@
 import { Schema, model, Types } from 'mongoose';
 import type { Document } from 'mongoose';
 
+import type { LangSuffixedKeyUnion } from 'src/schema/lang_utils.ts';
 import { create_dataloader_for_resource_by_primary_key_attr } from 'src/schema/loader_utils.ts';
+import {
+  make_foreign_id_type,
+  make_lang_suffixed_type,
+} from 'src/schema/mongoose_utils.ts';
 
-interface DatasetRulesInterface extends Document<Types.ObjectId> {}
-const DatasetMongooseSchema = new Schema<DatasetRulesInterface>({});
+interface ConditionInterface extends Document<Types.ObjectId> {
+  condition_type: string; // TODO this will be an enum once condition types are formalized
+  parameters?: (string | number | boolean)[];
+}
+const ConditionSchema = new Schema<ConditionInterface>({
+  condition_type: { type: String, required: true },
+  parameters: [{ type: Schema.Types.Mixed }],
+});
+
+interface RuleInterface
+  extends Document<Types.ObjectId>,
+    Record<LangSuffixedKeyUnion<`name`>, string>,
+    Record<LangSuffixedKeyUnion<`description`>, string> {
+  target_header: string;
+  column_type: string; // TODO this will be an enum once column types are formalized
+  conditions: ConditionInterface[];
+}
+const RuleSchema = new Schema<RuleInterface>({
+  ...make_lang_suffixed_type('name', { type: String, required: true }),
+  ...make_lang_suffixed_type('description', { type: String, required: true }),
+  target_header: { type: String, required: true },
+  column_type: { type: String, required: true },
+  conditions: [ConditionSchema],
+});
+
+interface DatasetRulesInterface extends Document<Types.ObjectId> {
+  rules: Record<string, RuleInterface>;
+  created_by: Types.ObjectId;
+  created_at: number;
+}
+const DatasetRulesMongooseSchema = new Schema<DatasetRulesInterface>({
+  rules: [RuleSchema],
+  created_by: make_foreign_id_type('User', { required: true }),
+  created_at: { type: Number, required: true },
+});
 export const DatasetRulesModel = model<DatasetRulesInterface>(
   'DatasetRules',
-  DatasetMongooseSchema,
+  DatasetRulesMongooseSchema,
 );
 
 export const DatasetRulesByIdLoader =
