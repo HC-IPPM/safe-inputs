641fa5605bbcd7347b87b81edaf19b7d48803c65
Stephen-ONeil <stephenlevioneil@gmail.com>
2024-07-18 13:57:59 -0400

    Add User schema fields for checking permissions (admin and ability to "own" collections)

--- api/src/schema/core/User/UserSchema.ts
+++ api/src/schema/core/User/UserSchema.ts
@@ -1,9 +1,13 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
 
-import { validate_user_is_super_user } from 'src/authz.ts';
+import {
+  validate_user_is_super_user,
+  validate_user_can_have_privileges,
+} from 'src/authz.ts';
 import { with_authz } from 'src/schema/resolver_utils.ts';
 
 import { UserModel, UserByEmailLoader } from './UserModel.ts';
+import type { UserDocument } from './UserModel.ts';
 
 export const UserSchema = makeExecutableSchema({
   typeDefs: `
@@ -22,6 +26,8 @@ export const UserSchema = makeExecutableSchema({
     created_at: Float!
     second_last_login_at: Float
     last_login_at: Float
+    is_admin: Boolean
+    can_own_collections: Boolean
   }
 `,
   resolvers: {
@@ -46,6 +52,34 @@ export const UserSchema = makeExecutableSchema({
           user: req?.user?.mongoose_doc,
         }),
       ),
+    },
+    User: {
+      is_admin: (
+        parent: UserDocument,
+        _args: unknown,
+        _context: unknown,
+        _info: unknown,
+      ) => {
+        try {
+          validate_user_is_super_user(parent);
+          return true;
+        } catch {
+          return false;
+        }
+      },
+      can_own_collections: (
+        parent: UserDocument,
+        _args: unknown,
+        _context: unknown,
+        _info: unknown,
+      ) => {
+        try {
+          validate_user_can_have_privileges(parent);
+          return true;
+        } catch {
+          return false;
+        }
+      },
     },
   },
 });
