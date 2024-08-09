c81b79df7cf731ea011e63fde4f984c545e5c2f2
Stephen Levi O'Neil <stephenlevioneil@gmail.com>
2024-08-09 01:40:42 -0400

    UI Jest/Lingui compatibility fix, and related test utilities (#447)
    * Fix the incompatability of the /ui Jest configuration with lingui, misc jest config cleanup and normalization between /ui and /api, add testing utils and mocks in ui/src/test_utils
    
    * Move LanguageButton.test.tsx from ui/test to ui/src/components, to be colocated with the src module. Implement actual tests in the file

---
+++ ui/babel.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-typescript',
+    // so that React doesn't need to be explicitly imported in every jsx file
+    ['@babel/preset-react', { runtime: 'automatic' }],
+  ],
+  plugins: ['macros'],
+};
