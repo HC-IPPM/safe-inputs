c78d975bc59200210ca3a4be95c875c970e3f150
Stephen-ONeil <stephenlevioneil@gmail.com>
2024-08-12 16:00:19 -0400

    Add utility components for a loading spinner and loading skeleton/block

---
+++ ui/src/components/Loading.tsx
@@ -0,0 +1,28 @@
+import { Spinner as ChakraSpinner, Skeleton } from '@chakra-ui/react';
+import type { SpinnerProps, SkeletonProps } from '@chakra-ui/react';
+import { t } from '@lingui/macro';
+import type { PropsWithChildren } from 'react';
+
+export const LoadingSpinner = (props: Omit<SpinnerProps, 'label'>) => (
+  <ChakraSpinner label={t`Loading...`} {...props} />
+);
+
+export const LoadingBlock = ({
+  children,
+  isLoading,
+  display = 'flex',
+  flexGrow = 1,
+  ...skeletonProps
+}: PropsWithChildren<
+  Omit<SkeletonProps, 'label' | 'isLoaded'> & { isLoading: boolean }
+>) => (
+  <Skeleton
+    isLoaded={!isLoading}
+    label={isLoading ? t`Loading...` : ''}
+    display={display}
+    flexGrow={flexGrow}
+    {...skeletonProps}
+  >
+    {children}
+  </Skeleton>
+);
