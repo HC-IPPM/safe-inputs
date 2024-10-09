5c3a0f872a395b9af7baee233e52e217766f5336
Stephen-ONeil <stephenlevioneil@gmail.com>
2024-10-09 13:18:15 -0400

    Move codegen configuration and dependencies to the repo root, as it doesn't strictly belong to either of just the /ui and /api sub-projects, it will likely make the most sense to run as part of the root level CI jobs, and it could potentially be extended with plugins that generate types for the /api in addition to the /ui projects

--- codegen.ts
+++ codegen.ts
@@ -1,12 +1,12 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  // TODO this is just for the docker env
-  schema: 'http://api:3000/api/graphql', // eslint-disable-line
-  documents: ['src/graphql/**/*.ts'],
+  schema:
+    process.env.GRAPHQL_ENDPOINT_URL ?? 'http://localhost:8080/api/graphql',
+  documents: ['ui/src/graphql/**/*.ts'],
   verbose: true,
   generates: {
-    'src/graphql/__generated__/': {
+    'ui/src/graphql/__generated__/': {
       preset: 'client',
       presetConfig: {
         gqlTagName: 'gql',
