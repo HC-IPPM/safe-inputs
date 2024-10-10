5b41f7a966988d63575ae6e8dd32c5f20724e84a
Stephen Levi O'Neil <stephenlevioneil@gmail.com>
2024-10-10 13:34:26 -0400

    graphql-codegen tweaks (#724)
    * Remove need for running postProcess.cjs after graphql-codegen. Configure codegen to output its ts imports with no extension specified (default), and configure rspack to look for .ts files in cases where no extension is specified. Eslint will enforce always specifying extensions still, outside of generated code
    
    * Add graphql-codegen outputs to prettierignore and eslint-ignored paths
    
    * Explicitly include @graphql-codegen/cli as a /ui dev-dependency. Rename the package.json script name to be clearer
    
    * Install optional graphql-codegen @parcel/watcher dependency, add script alias for running graphql-codegen in watch mode
    
    * Remove graphql-codegen output from .gitignore, will need it to be committed for the ci job's typechecking purposes. Don't want to just generate and check the types in the /ui CI job, because it's sensitive to changes in the /api dir. Better to generate and check in the types during dev, and likely confirm the types are valid from the common root level /cloudbuild.yaml (TODO)
    
    * Add gql-codegen to the docker dev environment
    
    * Move codegen configuration and dependencies to the repo root, as it doesn't strictly belong to either of just the /ui and /api sub-projects, it will likely make the most sense to run as part of the root level CI jobs, and it could potentially be extended with plugins that generate types for the /api in addition to the /ui projects
    
    * Add GraphQL Codegen checking as a step in the root CI pipeline.
    
    * Pin @graphql-codegen/cli and @parcel/watcher versions
    
    * Post rebase cleanup
    
    * In the docker-compose ui-gql-codegen service, wait for the api's graphql endpoint to be up before running graphql-codegen
    
    * Use eval in node-dev-entrypoint.once.sh to support multi-line docker cmd's
    
    * Correct image name for docker compose seed-dev-data service
    
    * Unclear source of exit code when root CI graphql codegen runs in cloudbuild, testing commit
    
    * Testing root cloud build GQL codegen test
    
    * The exit codes from the docker compose run in GraphQL Codegen check were 137 whether or not the gql-codegen check worked. Instead of running gql-codegen inside the compose environment itself and trying to get the exit code out, now spinning up the (slimmed down) docker compose environment in detached mode and then running the gql-codegen command from outside. Might not have node in the selected docker image though
    
    * Meant to use bash for the new GraphQL Codegen check script, not sh
    
    * Remove unnecessary (leftover) quote escaping in the GraphQL Codegen check script
    
    * Fix root cloud buil GraphQL Codegen check step
    
    * Add node script for waiting on the gql service to become available, make it a pre-step on the gql-codegen package.json scripts. Cleanup existing equivalents
    
    * Fix volume argument in the GraphQL Codegen check cloud build step
    
    * Increase wait time and retry count in the wait_for_gql_service script
    
    * Further increase the max retries in wait_for_gql_service.cjs to 15, on the last run it took exactly 10 retries (the old max) reach readiness
    
    * Remove the error I'd inserted in to the generated GQL types for testing purposes
    
    * Remove the root level postinstall script, move the "install all the things" to a root level package.json script called ci:all

--- codegen.ts
+++ codegen.ts
@@ -1,14 +1,12 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
-const GRAPHQL_API_URL =
-  process.env.GRAPHQL_API_URL || 'http://localhost:8080/api/graphql';
-
 const config: CodegenConfig = {
-  schema: GRAPHQL_API_URL,
-  documents: ['src/graphql/**/*.ts'],
+  schema:
+    process.env.GRAPHQL_ENDPOINT_URL ?? 'http://localhost:8080/api/graphql',
+  documents: ['ui/src/graphql/**/*.ts'],
   verbose: true,
   generates: {
-    'src/graphql/__generated__/': {
+    'ui/src/graphql/__generated__/': {
       preset: 'client',
       presetConfig: {
         gqlTagName: 'gql',
