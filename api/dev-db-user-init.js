eb92e7fc87defd54b512befe9cff6013555cdb7e
Stephen-ONeil <stephenlevioneil@gmail.com>
2024-10-11 14:52:20 -0400

    Configure the dev (and api test) docker compose environments to run the dev mongodb as a single-member replica set. Running as a replica set allows the local dev api to use mongo transactions, for better prod parity

---
+++ api/dev-db-user-init.js
@@ -0,0 +1,14 @@
+const { MDB_USERNAME, MDB_PW, MDB_NAME } = process.env;
+
+const api_db = db.getSiblingDB(MDB_NAME); // eslint-disable-line no-undef
+
+api_db.createUser({
+  user: MDB_USERNAME,
+  pwd: MDB_PW,
+  roles: [
+    {
+      role: 'readWrite',
+      db: MDB_NAME,
+    },
+  ],
+});
