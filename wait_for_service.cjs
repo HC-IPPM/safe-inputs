f2067163d0c2474e7b7d77c27c17bf74e5e53942
Stephen-ONeil <stephenlevioneil@gmail.com>
2024-10-21 18:06:57 -0400

    Rwork wait_for_gql_service.cjs to be endpoint generic, use to wait for the ui to be ready before running axe scans in the /ui cloudbuild

--- wait_for_service.cjs
+++ wait_for_service.cjs
@@ -3,20 +3,25 @@ const { setTimeout } = require('node:timers/promises');
 
 const retry_max = 30;
 const timeout_seconds = 5;
-const endpoint =
-  process.env.GRAPHQL_ENDPOINT_URL ?? 'http://localhost:3000/api/graphql';
+const endpoint = process.argv[2];
+
+if (endpoint === undefined) {
+  throw new Error(
+    'wait_for_service.cjs requires one argument, the endpoint URL to wait for.',
+  );
+}
 
 const try_fetch = (retry_count) =>
   fetch(endpoint)
     .then(({ status }) => {
       if (status === 200) {
-        console.log('Api service responded successfully!');
+        console.log(`Service at ${endpoint} responded successfully!`);
         process.exit(0);
       } else if (retry_count >= retry_max) {
-        console.log('Api service took too long, out of retries!');
+        console.log(`Service at ${endpoint} took too long, out of retries!`);
         process.exit(1);
       } else {
-        console.log('Api service response pending...');
+        console.log(`Service at ${endpoint} response pending...`);
         return setTimeout(timeout_seconds * 1000).then(() =>
           try_fetch(retry_count + 1),
         );
@@ -24,7 +29,7 @@ const try_fetch = (retry_count) =>
     })
     .catch((error) => {
       if (retry_count < retry_max) {
-        console.log('Api service response pending...');
+        console.log(`Service at ${endpoint} response pending...`);
         return setTimeout(timeout_seconds * 1000).then(() =>
           try_fetch(retry_count + 1),
         );
