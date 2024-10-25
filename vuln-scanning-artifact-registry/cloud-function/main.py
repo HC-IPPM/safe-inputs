7d799c33479a573eb7446ccff909cbf45f17d750
LilaKelland <lila.kelland@gmail.com>
2024-10-25 20:12:01 +0000

    Added links and filters for just vunerability types

--- vuln-scanning-artifact-registry/cloud-function/main.py
+++ vuln-scanning-artifact-registry/cloud-function/main.py
@@ -1,8 +1,19 @@
+
+# https://medium.com/@a.j.abbott24/google-cloud-surfacing-container-image-vulnerabilities-91dcf3f147f3
+# https://github.com/GoogleCloudPlatform/python-docs-samples/tree/main/containeranalysis/snippets
+# https://cloud.google.com/artifact-analysis/docs
+# https://cloud.google.com/artifact-analysis/docs/reference/rest
+# https://cloud.google.com/artifact-analysis/docs/reference/libraries
+# https://github.com/googleapis/google-cloud-python/tree/main/packages/google-cloud-containeranalysis
+# https://cloud.google.com/artifact-analysis/docs/create-notes-occurrences
+
 import base64
 import os
 import json
 from google.cloud.devtools import containeranalysis_v1
 from google.cloud import storage
+import functions_framework
+# from google.cloud.devtools.containeranalysis_v1.types import NoteKind 
 
 def write_vuln_to_bucket(bucket_name, vuln_text, destination_object_name):
     storage_client = storage.Client()
@@ -10,99 +21,58 @@ def write_vuln_to_bucket(bucket_name, vuln_text, destination_object_name):
     obj = bucket.blob(destination_object_name)
     obj.upload_from_string(vuln_text)
 
-def delete_vuln_from_bucket(bucket_name, destination_object_name):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    obj = bucket.blob(destination_object_name)
-    obj.delete()
-
 # @functions_framework.cloud_event
 def image_vuln_pubsub_handler(event, context):
-        #get the Pub/Sub message containing the vulnerability occurrence id
-    data = json.loads(base64.b64decode(event['data']).decode('utf-8'))
+    
+    # Get the Pub/Sub message containing the vulnerability occurrence id
+    data = json.loads(base64.b64decode(event['data']).decode('utf-8')) # 'data' contains 3 elements { name, kind, notifcation time}
 
-    #load in environment variables for GCS bucket.  
+    # Load in environment variables for GCS bucket.  
     # bucket_name = os.environ.get("BUCKET_NAME", "")
-    bucket_name = "my-bucket-of-risk"
+    bucket_name = 'my-bucket-of-risk'
 
     if bucket_name == '':
         print("Bucket name not set")
         return
 
-    # if data['kind'] == 'VULNERABILITY':
-    #      if 'name' in data:
-    #           occurrence_id=data['name']
-    # if occurrence_id == '':
-    #   return
-
-    #get the occurrence via the grafeas client
+    # Get the occurrence via the grafeas client
     occurrence_name = (data['name'])
     client = containeranalysis_v1.ContainerAnalysisClient()
     grafeas_client = client.get_grafeas_client()
     o = grafeas_client.get_occurrence(name=occurrence_name)
 
-    # Filter to process only VULNERABILITY occurrences
-    print(f"Occurrence kind: {occurrence.kind}")
-    
-    if occurrence.kind != containeranalysis_v1.enums.NoteKind.VULNERABILITY:
-        print(f"Skipping non-vulnerability occurrence: {occurrence_name}")
-        return
+    # print(f"Occurrence kind: {o.kind}") # this is numeric
+    print(f"Occurance content: {o}") 
+    # print(f"resource uri: {o.resource_uri}")
+    # print(f"occurance note name: {o.note_name}")
 
-    # Create a simple file name based on the occurrence ID
-    occurrence_id = occurrence_name.split('/')[-1]  # Extract the occurrence ID
-    destination_object_name = f"{occurrence_id}.jsob"  # Use the ID as the filename
+    if o.kind == 1: # Note o.kind of 1 = VUNERABILITY,  4 = PACKAGE, 6 = DISCOVERY
+        print('VULNERABILITY')
+        
+        # Extract components for filename (vunerability id, package - version)
+        vuln_id = o.note_name.split('/')[-1]
+        resource_name = o.resource_uri.split('/')[-1]
 
-    #write to storage
-    # write_vuln_to_bucket(bucket_name, str(o), str(o.name))
-    write_vuln_to_bucket(bucket_name, str(o), destination_object_name)
+        # # Extract short sha
+        # full_identifier = o.resource_uri.split('/')[-1] # includes container and full sha
+        # # Split into name and SHA
+        # container_name, full_sha = full_identifier.split(':')  # name is "sha256" and full_sha is the full SHA
+        # short_sha = full_sha[:12]  # Take the first 12 characters of the SHA
 
+         # Loop through package issues to get package details
+        for issue in o.vulnerability.package_issue:
+            affected_package = issue.affected_package
+            affected_version = issue.affected_version.name
+            # effective_severity = issue.effective_severity # This is numerical 
 
+        # Concatenate and save to bucket
+        occurance_filename = f"{vuln_id}-{affected_package}-{affected_version}-{resource_name}"
+        write_vuln_to_bucket(bucket_name, str(o), str(occurance_filename))
+        print(f"saved {occurance_filename} to gcs")
 
-
-#     #get the Pub/Sub message containing the vulnerability occurrence id
-#     data = json.loads(base64.b64decode(event['data']).decode('utf-8'))
-
-#     #load in environment variables for GCS bucket.  
-#     # bucket_name = os.environ.get("BUCKET_NAME", "")
-#     bucket_name = "my-bucket-of-risk"
-
-#     if bucket_name == '':
-#         print("Bucket name not set")
-#         return
-
-#     # Get the occurrence via the grafeas client
-#     occurrence_name = (data['name'])
-#     client = containeranalysis_v1.ContainerAnalysisClient()
-#     grafeas_client = client.get_grafeas_client()
-#     occurrence = grafeas_client.get_occurrence(name=occurrence_name)
-
-#     # Create a simple file name based on the occurrence ID, storing it at the root of the bucket
-#     occurrence_id = occurrence_name.split('/')[-1]  # Extract the occurrence ID
-#     destination_object_name = f"{occurrence_id}.json"  # Use the ID as the filename
-
-# #  # Check if the vulnerability has been resolved or fixed
-# #     # vulnerability_state = occurrence.vulnerability.effective_severity
-# #     # Or check for 'state' (if applicable)
-# #     if occurrence.vulnerability.state in ["DISMISSED", "FIXED"]:  # not ACTIVE
-# #         # Suffix the file with resolved/fixed
-# #         new_object_name = f"{occurrence_id}-{occurrence.vulnerability.state.lower()}.json"
-
-# #         # Delete the old active file
-# #         try:
-# #             delete_vuln_from_bucket(bucket_name, original_object_name)
-# #             print(f"Deleted active vulnerability: {original_object_name}")
-# #         except Exception as e:
-# #             print(f"Failed to delete active vulnerability: {e}")
-
-# #         # Write the new resolved/fixed vulnerability to the bucket
-# #         write_vuln_to_bucket(bucket_name, str(occurrence), new_object_name)
-# #         print(f"Saved resolved/fixed vulnerability as: {new_object_name}")
-
-# #     else:
-# #         # Write or update the vulnerability information in the bucket
-# #         write_vuln_to_bucket(bucket_name, str(occurrence), destination_object_name)
-# #         print(f"Updated vulnerability: {occurrence_id}")
-
-#     #write to storage
-#     # write_vuln_to_bucket(bucket_name, str(o), str(o.name))
-#     write_vuln_to_bucket(bucket_name, str(o), destination_object_name)
+    elif o.kind == 4:
+        print('PACKAGE')
+    elif o.kind == 6:
+        print('DISCOVERY')
+    else:
+        print(o.kind)
