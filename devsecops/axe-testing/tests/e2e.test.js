441cabcf51b7cc5b8a022edc3d6ad4f431c864e2
LilaKelland <lila.kelland@gmail.com>
2024-11-08 13:33:00 +0000

    changed homepage url and removed old commented out lines

--- devsecops/axe-testing/tests/e2e.test.js
+++ devsecops/axe-testing/tests/e2e.test.js
@@ -1,47 +1,10 @@
 import { runAccessibilityScan } from '../index.js';
 
-// describe('End-to-End Accessibility Test', () => {
-//   it('should crawl and complete the accessibility for more than one pages', async () => {
-//     process.env.HOMEPAGE_URL = 'http://localhost:8080/accessible.html'; // Start this page first, then should crawl to other
-
-//     // Run the accessibility scan and capture the result
-//     const isSafeInputs = false; // Skips the Safe Inputs specific login steps
-//     const {
-//       urlsWithViolations,
-//       filteredResults
-//     } = await runAccessibilityScan(isSafeInputs);
-
-//   // Check that the scan crawled more than one page
-//   const hasAccessiblePage = filteredResults.some(
-//     (entry) => entry.url === 'http://127.0.0.1:8080/accessible.html'
-//   );
-
-//   const hasInaccessiblePage = filteredResults.some(
-//     (entry) => entry.url === 'http://127.0.0.1:8080/inaccessible.html'
-//   );
-
-//   expect(hasAccessiblePage).toBe(true);
-//   expect(hasInaccessiblePage).toBe(true);
-
-//   // Check the accessible page has no violations
-//   const accessiblePageViolations = urlsWithViolations.find(
-//     (entry) => entry[0] === 'http://127.0.0.1:8080/accessible.html'
-//   );
-//   expect(accessiblePageViolations).toBeUndefined();
-
-//   // Check the inaccessible page has violations
-//   const hasInaccessiblePageViolations = urlsWithViolations.some(
-//     (entry) => entry[0].includes('http://127.0.0.1:8080/inaccessible.html')
-//   );
-//   expect(hasInaccessiblePageViolations).toBe(true);
-//   });
-// });
-
 describe('End-to-End Accessibility Test', () => {
   let scanResults;
 
   beforeAll(async () => {
-    process.env.HOMEPAGE_URL = 'http://localhost:8080/accessible.html'; // Start with the first page
+    process.env.HOMEPAGE_URL = 'http://localhost:8080';
     const isSafeInputs = false; // Skips the Safe Inputs specific login steps
     scanResults = await runAccessibilityScan(isSafeInputs);
   });
