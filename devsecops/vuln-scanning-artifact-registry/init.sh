9cd122ae42436173029367c8e9cf2afd8d1c89c2
LilaKelland <lila.kelland@gmail.com>
2024-11-14 20:56:36 +0000

    updated init.sh after testing in new project

--- devsecops/vuln-scanning-artifact-registry/init.sh
+++ devsecops/vuln-scanning-artifact-registry/init.sh
@@ -1,81 +1,77 @@
-# https://medium.com/google-cloud/centrally-managing-artifact-registry-container-image-vulnerabilities-on-google-cloud-part-one-d86fb4791601
-# https://cloud.google.com/run/docs/tutorials/eventarc-functions
+# ARTIFACT REGISTRY VUNERABILITIES 
+#   Here we're setting up the resources in order to copying vunerabilities detected from Artifact Registry 
+#   vunerability scanning to a Google Cloud Storage bucket using a built in Artifact registry occurance pubsub topic
 
-# # Commented out for now as using for running piecemeal
+# This will be done with Config Connector, but using gcloud (in a separate project for now) to determine resources to provision
+
+# REFERENCES:
+#   https://medium.com/google-cloud/centrally-managing-artifact-registry-container-image-vulnerabilities-on-google-cloud-part-one-d86fb4791601
+#   https://cloud.google.com/run/docs/tutorials/eventarc-functions
+
+
+
+# ---- SET ENV VARIABLES (commented out as used when run piece-meal)
 # export PROJECT_ID=phx-01hwmw2c1r4
-# export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(projectNumber)")
-# export BUCKET_NAME=my-bucket-of-risk
-# export VULN_CLOUD_FUNCTION_SERVICE_ACCOUNT=vuln-cloud-function-sa
-# export REPO_NAME=hello-world-repo
+export PROJECT_ID=phx-01jck529wqj
+export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(projectNumber)")
+export BUCKET_NAME=my-bucket-of-risk-the-third
+export VULN_CLOUD_FUNCTION_SERVICE_ACCOUNT=filter-vuln-gcf-sa
+export REPO_NAME=risky-images-repo
+export REGION=northamerica-northeast1
 
-# Set ENV variables for cloud function
 PROJECT_ID=phx-01hwmw2c1r4
 PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(projectNumber)")
-BUCKET_NAME=my-bucket-of-risk-the-second
-VULN_CLOUD_FUNCTION_SERVICE_ACCOUNT=vuln-cloud-function-sa
+BUCKET_NAME=my-bucket-of-risk-the-third
+VULN_CLOUD_FUNCTION_SERVICE_ACCOUNT=filter-vuln-gcf-sa
+REPO_NAME=risky-images-repo
+REPO_NAME=risky-images-repo
+REGION=northamerica-northeast1
 
-# Create bucket
-gsutil mb -l northamerica-northeast1 -p $PROJECT_ID gs://$BUCKET_NAME
 
-# Set project (to save inputs (or mine at the moment))
+
+# ---- SET PROJECT (to test project at the moment))
 gcloud config set project $PROJECT_ID
 
-# Enable services: artifact registry, scanning of registry, visibiliity into metadata (occurances) and cloudbuild
+
+
+# ----- ENABLE SERVICES  artifact registry, scanning of registry, visibiliity into metadata (occurances) and cloudbuild
+#   event-driven serverless (cloud run) function, and storage to store results in bucket 
 gcloud services enable cloudresourcemanager.googleapis.com \
     artifactregistry.googleapis.com \
-    services enable cloudbuild.googleapis.com \
+    cloudbuild.googleapis.com \
+    eventarc.googleapis.com \
+    run.googleapis.com \
+    storage.googleapis.com \
+    pubsub.googleapis.com \
+    containeranalysis.googleapis.com \
+    cloudkms.googleapis.com \
+    cloudfunctions.googleapis.com \
     containerscanning.googleapis.com
 
-# Enable services: event-driven serverless (cloud run) function, and to store results in bucket 
-gcloud services enable eventarc.googleapis.com \
-    run.googleapis.com \
-    storage.googleapis.com
+# check with services have been enabled 
+gcloud services list --enabled
 
-    # (uses "service-$PROJECT_NUMBER@gcf-admin-robot.iam.gserviceaccount.com")
-
-# ----------- ROLES ------------------
-# $PROJECT_NUMBER-compute@developer.gserviceaccount.com these roles: Artifact Registry Create-on-Push Writer
-# Cloud Run Developer
-# Compute Load Balancer Admin
-# Compute Viewer
-# Editor
-# Eventarc Event Receiver
-# Logs Bucket Writer
-# Logs Writer
-# Service Account User
-# Storage Admin
-# Storage Object Admin
-
-# vuln-cloud-function-sa@$PROJECT_ID.iam.gserviceaccount.com
-# Container Analysis Occurrences Viewer
-
-# service-$PROJECT_NUMBER@gcf-admin-robot.iam.gserviceaccount.com 
-# Cloud Functions Service Agent
-
-# service-744920990938@gcp-sa-containerscanning.iam.gserviceaccount.com
-# Container Scanner Service Agent
-
-# service-744920990938@gcp-sa-eventarc.iam.gserviceaccount.com
-# Eventarc Service Agent
-
-# service-744920990938@gcp-sa-pubsub.iam.gserviceaccount.com
-# Cloud Pub/Sub Service Agent
-# Service Account Token Creator
-
-# service-744920990938@gcp-sa-runapps.iam.gserviceaccount.com
-# Serverless Integrations Service Agent
-
-# service-744920990938@gcp-sa-cloudbuild.iam.gserviceaccount.com
-# Cloud Build Service Agent
-
-# service-744920990938@serverless-robot-prod.iam.gserviceaccount.com
-# Cloud Run Service Agent
-
-# service-744920990938@gs-project-accounts.iam.gserviceaccount.com
-# Pub/Sub Publisher
+# # To undo (doesnt seem to remove the associated google generated service account)
+# gcloud services disable cloudresourcemanager.googleapis.com \
+#     artifactregistry.googleapis.com \
+#     cloudbuild.googleapis.com \
+#     eventarc.googleapis.com \
+#     run.googleapis.com \
+#     storage.googleapis.com \
+#     pubsub.googleapis.com \
+#     containeranalysis.googleapis.com \
+#     cloudkms.googleapis.com \
+#     cloudfunctions.googleapis.com \
+#     containerscanning.googleapis.com
 
 
-# ---- IAM ----
+
+# ----- CREATE BUCKET to store vunerablity occurances
+gsutil mb -l northamerica-northeast1 -p $PROJECT_ID gs://$BUCKET_NAME
+
+
+
+# ---- IAM 
 # Service account the cloud function runs under
 gcloud iam service-accounts create $VULN_CLOUD_FUNCTION_SERVICE_ACCOUNT \
 --description="Service Account to create process image scan vulnerabilities" \
@@ -86,40 +82,25 @@ gcloud projects add-iam-policy-binding $PROJECT_ID \
     --member=serviceAccount:$VULN_CLOUD_FUNCTION_SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com \
     --role=roles/containeranalysis.occurrences.viewer
 
-# And Object Creator (on bucket we just created) 
+# And add Object Creator role to vuln sa (on bucket we just created) 
 gsutil iam ch serviceAccount:$VULN_CLOUD_FUNCTION_SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com:objectCreator gs://$BUCKET_NAME
 
-# ---- to test with vunerable container image (nginx)
-# ---- Artifact Registry -----
-# Create Artifact Registry (we don't need to do this as there's one already in Safe inputs)
-gcloud artifacts repositories create $REPO_NAME \
- --location=northamerica-northeast1 \ 
- --repository-format=docker \
- --project=$PROJECT_ID
-
-# # ---- Pub Sub ----
-# gcloud pubsub topics create container-analysis-occurrences-v1 --project=$PROJECT_ID
-# I don't think we need to do this - this automatically is created with vunerabiltiy scanning in Artifact Registry 
-
-# ----- Cloud Function ----
-# *** need to change the trigger of pub sub cloud function (tutorial has click ops to do this )
-# enable cloud run functions API
-# https://cloud.google.com/blog/products/serverless/google-cloud-functions-is-now-cloud-run-functions
 
 
-export REGION=northamerica-northeast1
-gcloud config set run/region ${REGION}
-gcloud config set run/platform managed
-gcloud config set eventarc/location ${REGION}
+# ---- CREATE PUBSUB TOPIC ----
+gcloud pubsub topics create container-analysis-occurrences-v1 --project=$PROJECT_ID
 
-# grant roles for $PROJECT_NUMBER-compute@developer.gserviceaccount.com:
 
-# roles/artifactregistry.createOnPushWriter on phx-01hwmw2c1r4
-# roles/logging.logWriter on phx-01hwmw2c1r4
-# roles/storage.objectAdmin on phx-01hwmw2c1r4
 
-run invoker ?
-
+# ---- DEPLOY CLOUD FUNCTION (this will be done with cloud build in the future)
+#   Node (https://cloud.google.com/sdk/gcloud/reference/functions/deploy)
+gcloud functions deploy imageVulnPubSubHandler \
+    --source ./cloud-function/node \
+    --runtime nodejs18 \
+    --trigger-topic container-analysis-occurrences-v1 \
+    --entry-point imageVulnPubSubHandler \
+    --set-env-vars BUCKET_NAME=$BUCKET_NAME \
+    --region=$REGION
 # https://cloud.google.com/run/docs/configuring/cpu
 
 # # Python
@@ -131,29 +112,21 @@ run invoker ?
 #     --entry-point image_vuln_pubsub_handler \
 #     --region=northamerica-northeast1
 
-# Node (https://cloud.google.com/sdk/gcloud/reference/functions/deploy)
-gcloud functions deploy imageVulnPubSubHandler \
-    --source ./cloud-function/node \
-    --runtime nodejs18 \
-    --trigger-topic container-analysis-occurrences-v1 \
-    --entry-point imageVulnPubSubHandler \
-    --set-env-vars BUCKET_NAME=my-bucket-of-risk-the-second \
-    --region=northamerica-northeast1
+# ---- TEST
+# Create Artifact Registry Repo to store image (we don't need to do this as there's one already in Safe inputs)
+gcloud artifacts repositories create $REPO_NAME \
+ --location=northamerica-northeast1 \
+ --repository-format=docker  \
+ --project=$PROJECT_ID
 
-# This gets pushed to gcf-artifacts (and is also scanned when up and running)
-
-# ** need service account - right now using compute engine
-
-
-# ------ Push image with lots of vunerabilities (nginx - ie Dockerfile in this dir) to Artifact Registry to test
-REPO_NAME=cloud-function-testing3
-PROJECT_ID=phx-01hwmw2c1r4
-export REPO_NAME=cloud-function-testing-4
-export PROJECT_ID=phx-01hwmw2c1r4
+# Push image with vunerabilities to test (nginx) (nginx - ie Dockerfile in this dir) to Artifact Registry to test
 gcloud auth configure-docker northamerica-northeast1-docker.pkg.dev
 docker build --tag nginx .
 docker tag nginx northamerica-northeast1-docker.pkg.dev/$PROJECT_ID/$REPO_NAME/nginx-test
 docker push northamerica-northeast1-docker.pkg.dev/$PROJECT_ID/$REPO_NAME/nginx-test
+
+
+
 
 
 
